7feddabf1813e97369b2ff22893f09b8c0d89c05
unicesng <eunice.sng.2022@scis.smu.edu.sg>
2024-04-06 22:01:42 +0800

    Metrics Summary Table

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -1,7 +1,8 @@
 import AdminHeader from "./components/AdminHeader";
 import TargetsCard from "./components/TargetsCard";
-import DropdownList from "./components/DropdownList";
+import FrequencyDropdown from "./components/FrequencyDropdown";
 import { Card, Checkbox, Label } from "flowbite-react";
+import MetricsSummaryTable from "./components/MetricsSummaryTable";
 
 const targetData = [
   {
@@ -24,76 +25,80 @@ const targetData = [
     status: "bg-green-500",
     targetStat: 12,
     change: 0.2,
-  }
+  },
 ];
 
 function Dashboard() {
-    return (
-      <>
-        <div>
-          <AdminHeader title="Admin Dashboard" />
-          <div className="m-5">
-            <div className="flex justify-between">
-              <h3 className="m-3 mb-4 text-l font-extrabold leading-none tracking-tight text-gray-900 md:text-xl lg:text-2xl dark:text-white">
-                Targets Overview
-              </h3>
-              <DropdownList items={["This month", "This year", "This week"]} />
-            </div>
-
-            <div className="flex justify-around">
-              {targetData?.map((target) => {
-                return (
-                  <TargetsCard
-                    key={target.metricName}
-                    metricName={target.metricName}
-                    metricStat={target.metricStat}
-                    status={target.status}
-                    targetStat={target.targetStat}
-                    change={target.change}
-                  />
-                );
-              })}
-            </div>
+  return (
+    <>
+      <div>
+        <AdminHeader title="Admin Dashboard" />
+        <div className="m-5">
+          <div className="flex justify-between">
+            <h3 className="m-3 mb-4 text-l font-extrabold leading-none tracking-tight text-gray-900 md:text-xl lg:text-2xl dark:text-white">
+              Targets Overview
+            </h3>
+            <FrequencyDropdown></FrequencyDropdown>
           </div>
-          <div className="m-5 flex justify-around">
-            <Card className="w-1/2 m-3">
-              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Admin Tasks Remaining
-              </h5>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                <Checkbox />
-                <Label htmlFor="remember">
-                  {" "}
-                  &emsp; Fill in Company Profile
-                </Label>
-              </p>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                <Checkbox />
-                <Label htmlFor="remember">
-                  {" "}&emsp; Select relevant ESG metrics{" "}
-                </Label>
-              </p>
-            </Card>
-            <Card className="w-1/2 m-3">
-              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Get Personalised Reccomendation
-              </h5>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                <Checkbox />
-                <Label htmlFor="remember">
-                  {" "}&emsp; Set targets for each metric
-                </Label>
-              </p>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                <Checkbox />
-                <Label htmlFor="remember">
-                  {" "}&emsp; Disseminate to relevant departments{" "}
-                </Label>
-              </p>
-            </Card>
+          <div className="flex justify-around">
+            {targetData?.map((target) => {
+              return (
+                <TargetsCard
+                  key={target.metricName}
+                  metricName={target.metricName}
+                  metricStat={target.metricStat}
+                  status={target.status}
+                  targetStat={target.targetStat}
+                  change={target.change}
+                />
+              );
+            })}
           </div>
         </div>
-      </>
-    );
+        <div className="m-5 flex justify-around">
+          <Card className="w-1/2 m-3">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Admin Tasks Remaining
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              <Checkbox />
+              <Label htmlFor="remember"> &emsp; Fill in Company Profile</Label>
+            </p>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              <Checkbox />
+              <Label htmlFor="remember">
+                {" "}
+                &emsp; Select relevant ESG metrics{" "}
+              </Label>
+            </p>
+          </Card>
+          <Card className="w-1/2 m-3">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Get Personalised Reccomendation
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              <Checkbox />
+              <Label htmlFor="remember">
+                {" "}
+                &emsp; Set targets for each metric
+              </Label>
+            </p>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              <Checkbox />
+              <Label htmlFor="remember">
+                {" "}
+                &emsp; Disseminate to relevant departments{" "}
+              </Label>
+            </p>
+          </Card>
+        </div>
+        <div className="m-5">
+          <Card className="w-full">
+            <MetricsSummaryTable />
+          </Card>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Dashboard;
--- frontend/src/components/AdminHeader.tsx
+++ frontend/src/components/AdminHeader.tsx
@@ -11,12 +11,10 @@ const AdminHeader: React.FC<TitleProps> = ({ title }) => {
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center">
           <div className="flex justify-start items-center">
-            <a href="https://flowbite.com" className="flex mr-4">
-              <img src={Logo} className="mr-3 h-8" alt="Laptop Boi Logo" />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                {title}
-              </span>
-            </a>
+            <img src={Logo} className="mr-3 h-8" alt="Laptop Boi Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              {title}
+            </span>
             <form action="#" method="GET" className="hidden lg:block lg:pl-2">
               <div className="relative mt-1 lg:w-96">
                 <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
