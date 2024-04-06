f0e30bda727c9131da2e19982e8f3b367dc1b59d
Tan How Suen <howsuen@gmail.com>
2024-04-06 21:37:45 +0800

    Update api

--- backend/src/api.py
+++ backend/src/api.py
@@ -2,6 +2,10 @@ import os
 from flask import Flask, request
 from openai import AzureOpenAI
 from dotenv import dotenv_values
+import requests
+import re
+from bs4 import BeautifulSoup
+
 config = dotenv_values(".env")
     
 client = AzureOpenAI(
@@ -11,16 +15,15 @@ client = AzureOpenAI(
     )
 
 app = Flask(__name__)
-    
-deployment_name='gpt-35-turbo' 
 
+DEPLOYMENT_NAME='gpt-35-turbo' 
 INSTRUCTIONS = open("./src/training_file.txt", "r").read()
 FAKE_INPUT_METRICS = open("./src/input_metrics.txt", "r").read()
 DEFAULT_COMPANY = "Fast Retailing"
 DEFAULT_TODO = "State the name of the standard. Give me suggestions to improve my company's sustainability practices based on my company's ESG report."
 IGNORE_IRRELAVANCE = "REMEMBER THIS: IF THE USER ASKS AN IRRELEVANT QUESTION OR SOMETHING THAT IS NOT RELATED TO ESG, YOU MUST REPLY WITH 'Sorry, unknown request.'"
 
-@app.route("/recommend", methods=["POST"])
+@app.route("/recommend", methods=["GET"])
 def generate_recommendations():
     data = request.get_json()
 
@@ -36,7 +39,7 @@ def generate_recommendations():
         todo = data.get("todo")
 
     response = client.chat.completions.create(
-        model=deployment_name,
+        model=DEPLOYMENT_NAME,
         messages=[
             {"role": "system", "content": INSTRUCTIONS},
             {"role": "user", "content": f"My company is called {company_name}. This is my company's ESG report:\n {input_metrics}"},
@@ -47,6 +50,30 @@ def generate_recommendations():
     )
 
     return response.choices[0].message.content
+
+@app.route('/findIndustry/<company>', methods=['GET'])
+def findIndustry(company):
+    # print(company)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        "Content-Type": "text/html; charset=utf-8",
+    }
+    response = requests.get(f'https://sasb.ifrs.org/wp-json/sasb/v1/sicsSearch?s={company}&locale=en-gb&format=html&page=1&limit=10', headers=headers)
+    result = response.text
+    result = unicode_to_html(result)
+    # return result
+    soup = BeautifulSoup(result, 'html.parser')
+    td_contents = re.findall(r'<td>(.*?)<', result)
+    industries = []
+    seen = set()
+    for i in range(len(td_contents)):
+        if i % 6 == 4 and td_contents[i] not in seen:
+            industries.append(td_contents[i])
+            seen.add(td_contents[i])
+    return industries
+
+def unicode_to_html(input):
+    return re.sub(r'[\u007F-\uFFFF]', lambda character: "&#" + str(ord(character.group())) + ";", input.replace(" ", ""))
 
 if __name__ == "__main__":
     app.run(debug=True)
