f3d4868beff7c6e1b7e473cd2ab5cc985f70a8af
Lee Shuoan <leeshuoan38@gmail.com>
2024-04-06 15:36:42 +0800

    cleaned up frontend

---
+++ frontend/src/Landing.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div>
+        LAPTOP BOIS
+      </div>
+    </>
+  );
+}
+
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./Nav.tsx";
+import Landing from "./Landing.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+);
